1c7b736cf5d0377c205e6d28fa8df848e0706eb7
liulan <374472569@qq.com>
2022-05-25 09:32:08 +0800

    add io in App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script>
+import io from '@hyoga/uni-socket.io'
+
 export default {
   onLaunch: function () {
     console.log('App Launch1111')
+    const socket = io(`wss://192.168.100.7:8844}`, {
+      path: '/message',
+      transports: ['websocket', 'polling'],
+    })
+    // 处理刷新页面,再发一次登录的指令
+    socket.emit(
+      'login',
+      JSON.stringify({
+        agent: 'bandex',
+        username: '483231',
+        source: 'app',
+      })
+    )
   },
   onShow: function () {
     console.log('App Show')
